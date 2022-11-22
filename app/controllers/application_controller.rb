097b019751b11da4e44a8b5f07656914946dd2a3
saungoomyat <93793082+saungoomyat@users.noreply.github.com>
2022-11-21 22:02:30 -0500

    Add files via upload

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,57 @@
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  # Add your routes here
+  get "/" do
+    { message: "Welcome to my project!" }.to_json
+  end
+
+  get '/calenders' do
+    calender = Calender.all
+    calender.to_json
+  end
+
+  get "/calenders/:id" do
+    calender = Calender.find(params[:id])
+    calender.to_json(include: :activities)
+  end
+
+
+
+  get "/activities" do
+    activity = Activity.all
+    activity.to_json()
+  end
+
+  get "/activities/:id" do
+    activity = Activity.find(params[:id])
+    activity.to_json()
+  end
+
+  post '/activities' do
+    activity = Activity.create(
+      to_do: params[:to_do],
+      calender_id: params[:calender_id],
+      location: params[:location],
+      duration: params[:duration])
+    activity.to_json
+  end
+
+  delete '/activities/:id' do
+    activity = Activity.find(params[:id])
+    activity.destroy
+    activity.to_json
+  end 
+
+  patch '/activities/:id' do
+    activity = Activity.find(params[:id])
+    activity.update(
+      to_do: params[:to_do],
+      calender_id: params[:calender_id],
+      location: params[:location],
+      duration: params[:duration])
+    activity.to_json
+  end
+
+
+  
+end
